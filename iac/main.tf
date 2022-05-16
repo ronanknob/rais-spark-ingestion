05b4444f35f21955f8791580b10aa65b3fc33d97
ronanknob <ronan.knob@gmail.com>
2022-05-15 23:21:42 -0300

    Add spark job

--- iac/main.tf
+++ iac/main.tf
@@ -1,7 +1,13 @@
-
-
 resource "aws_s3_object" "refined_zone" {
     bucket = "${var.base_bucket_name}"
     acl    = "private"
     key    = "refined_zone/"
+}
+
+resource "aws_s3_object" "codigo_processamento_rais" {
+    bucket = "${var.base_bucket_name}"
+    key = "emr-code/job_spark.py"
+    acl = "private"
+    source = "../emr-jobs/process_rais.py"
+    etag = filemd5( "../emr-jobs/process_rais.py")
 }
